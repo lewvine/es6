597693fb086855a041a65ddd2ab300a2da719580
Lewis Vine <lew.vine@gmail.com>
2024-07-24 15:38:06 -0400

    Some examples of let v var

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 //Nothing here yet.
+
+let name = 'Max';
+
+function greet(){
+    let age = 30;
+    let name = 'Manuel';
+    console.log(name, age);
+}
+
+console.log(name);
+greet();
+console.log(name);
